353cd0eaa5a4c5f10e9e399c95b9183350026510
Thomas Lambert <tjalambert@gmail.com>
2017-01-17 01:51:04 +0000

    Added ping and help commands

--- MuBot.js
+++ MuBot.js
@@ -9,7 +9,34 @@ const _BOT = new Discord.Client()
 const _TOKEN = INSERTYOURTOKEN
 const _PREFIX = "+mu"
 
-var commands = {}
+var commands = {
+  "ping": {
+    usage: "+mu ping",
+		description: "Responds with pong if the bot is alive",
+		process: function(bot, msg, suffix) {
+		  msg.reply("Pong!")
+		}
+	},
+  "help": {
+    usage: "+mu help",
+		description: "Lists all commands",
+		process: function(bot, msg, suffix) {
+		  let msgOut = "Help:" +'\n```'
+			for (let cmd in commands) {
+			  msgOut += '\n' + addSpaces(cmd, 8) + "| " + commands[cmd].usage
+      }
+      msgOut += "```"
+      msg.channel.sendMessage(msgOut)
+    }
+	},
+	"q": {
+		usage: "+mu q <video link>",
+		description: "Adds a video to the music queue",
+		process: function(bot, msg, suffix) {
+		  //TODO
+		}
+	},
+}
 
 _BOT.on('ready', () => {
   console.log('MuBot ready!')
@@ -18,7 +45,23 @@ _BOT.on('ready', () => {
 _BOT.on('message', msg => {
   //The bot ignores messages that don't start with the prefix or are from a bot
 	if (!msg.content.startsWith(_PREFIX) || msg.author.bot) {return}
+	
+	let msgCmd = msg.content.split(" ")[1]
+  
+  for (let cmd in commands) {
+    if (msgCmd === cmd) {
+      let msgSuffix = msg.content.replace(_PREFIX + " " + msgCmd + " ", "")
+      commands[cmd].process(_BOT, msg, msgSuffix)
+    }
+  }
   
 })
+
+function addSpaces(str, size) {
+  while (str.length < size) {
+    str += " "
+  }
+  return str
+}
 
 _BOT.login(_TOKEN)
